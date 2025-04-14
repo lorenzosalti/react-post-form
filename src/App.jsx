da380d158682070b9e4226651406e71882b03f32
lorenzosalti <lorenzosalti@gmail.com>
2025-04-14 16:36:28 +0200

    feat: created author and title input fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
 
+import { useState } from "react"
 
 function App() {
+
+  const [postData, setPostData] = useState({
+    author: "",
+    title: ""
+  })
+
+  function handlePostData(event) {
+    setPostData(prev => ({
+      ...prev,
+      [event.target.name]: event.target.value
+    }))
+  }
 
 
   return (
     <>
-      <h1></h1>
+      <h1>Inserimento nuovo post</h1>
+
+      <hr />
+
+      <form>
+
+        <input
+          type="text"
+          name="author"
+          value={postData.author}
+          onChange={handlePostData}
+          placeholder="Autore del post"
+        />
+
+
+        <input
+          type="text"
+          name="title"
+          value={postData.title}
+          onChange={handlePostData}
+          placeholder="Titolo del post"
+        />
+
+      </form>
+
     </>
   )
 }
